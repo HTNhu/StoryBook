508421d837df8eb935bc808331ca3b8f1a157001
HTNhu <hotrannhu@gmail.com>
2019-11-13 11:24:07 +0700

    theme

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,12 @@
-import { configure, addDecorator } from '@storybook/react';
+import { configure, addDecorator, addParameters } from '@storybook/react';
 import React from 'react';
+import myTheme from './myTheme';
+
+addParameters({
+  options: {
+    theme: myTheme,
+  },
+});
 addDecorator(storyFn => <div style={{ textAlign: 'center' }}>{storyFn()}</div>);
 // automatically import all files ending in *.stories.js
 configure(require.context('../src', true, /\.stories\.js$/), module);
